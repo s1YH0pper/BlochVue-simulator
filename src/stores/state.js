f101e0df2fb7d04aa74d5af8f3a21491b3f0cfee
s1YH0pper <1147083390@qq.com>
2025-10-10 01:15:10 +0800

    Fix: improve state saving and restoration logic in appState

--- src/stores/state.js
+++ src/stores/state.js
@@ -65,8 +65,8 @@ export const useAppStateStore = defineStore("appState", {
     actions: {
         saveState() {
             const state = useStateStore();
-            this.savedState = { ...state };
-            delete this.savedState.IsocArr
+            this.savedState = JSON.parse(JSON.stringify(state.$state));
+            delete this.savedState.IsocArr;
 
             this.savedState2 = state.IsocArr.map(item => ({
                 Mx: item.M.x,
@@ -79,7 +79,7 @@ export const useAppStateStore = defineStore("appState", {
 
         restoreState() {
             const state = useStateStore();
-            Object.assign(state, this.savedState);
+            state.$patch({ ...this.savedState });
 
             state.IsocArr.forEach((item, index) => {
                 if (index >= this.savedState2.length) {
